36a270a4aa2ed691c0b4f031187cfe64100fcb16
Wafi Afdi Alfaruqhi <wafiafdi@gmail.com>
2023-11-16 16:05:52 +0700

    updt : Visual_Object namespace untuk fungsi"

--- Visual_Object/Class1.cs
+++ Visual_Object/Class1.cs
@@ -7,23 +7,79 @@ using System.Threading.Tasks;
 namespace Visual_Object
 {
 
-
-
-    public abstract class TargetObject
+    public enum VisualTargetSelection
     {
-        private enum VisualTarget
+        Potato,
+        Grape,
+    }
+
+    public class TargetObject
+    {
+
+
+        private VisualTargetSelection objectTarget;
+        private int[] targetRange = new int[6];
+
+        public void SettVisualTargetSelection (VisualTargetSelection selection)
         {
-            Potato,
-            Grape,
+            objectTarget= selection;    
         }
 
-        private VisualTarget objectTarget;
-        private int[] TargetColor = new int[3];
-
-        private void test()
+        public VisualTargetSelection GetTargetVisualSelection ()
         {
-            System.Console.WriteLine("Test");//Console is not found in system
+            return objectTarget;
         }
+
+        public int[] GetTargetColor ()
+        {
+            return targetRange;
+        }
+
+        public void SetTargetColor()
+        {
+            int h_channel_min;
+            int s_channel_min;
+            int v_channel_min;
+            int h_channel_max;
+            int s_channel_max;
+            int v_channel_max;
+
+            switch(this.objectTarget)
+            {
+                case VisualTargetSelection.Potato:
+                    h_channel_min = 0;
+                    s_channel_min = 1;
+                    v_channel_min = 2;
+                    h_channel_max = 3;
+                    s_channel_max= 4;
+                    v_channel_max = 5;
+                    break;
+                case VisualTargetSelection.Grape:
+                    h_channel_min = 0;
+                    s_channel_min = 1;
+                    v_channel_min = 2;
+                    h_channel_max = 3;
+                    s_channel_max = 4;
+                    v_channel_max = 5;
+                    break;
+                default:
+                    h_channel_min = 0;
+                    s_channel_min = 1;
+                    v_channel_min = 2;
+                    h_channel_max = 3;
+                    s_channel_max = 4;
+                    v_channel_max = 5;
+                    break;
+            }
+
+            this.targetRange[0] = h_channel_min;
+            this.targetRange[1] = s_channel_min;
+            this.targetRange[2] = v_channel_min;
+            this.targetRange[3] = h_channel_max;
+            this.targetRange[4] = s_channel_max;
+            this.targetRange[5] = v_channel_max;
+        }
+
     }
 
 
