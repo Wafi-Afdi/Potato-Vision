bd478009296c30109c8ef80388861976acac8ee7
Wafi-Afdi <wafiafdi@gmail.com>
2023-11-26 16:28:32 +0700

    add ; library class untuk objek yang ditarget

---
+++ Potato-Vision/EnumBindingExtension.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+using Visual_Object;
+
+namespace Potato_Vision
+{
+    public class EnumBindingExtension : MarkupExtension 
+    {
+        public Type EnumType { get; private set; }
+
+        public EnumBindingExtension(Type enumType)
+        {
+            if (enumType is null || !enumType.IsEnum)
+            {
+                throw new Exception("Enum Type must not be a null type");
+            } 
+
+            EnumType = enumType;
+        }
+
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return Enum.GetValues(EnumType);
+        }
+    }
+}
